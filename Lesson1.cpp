8dbc49e5e0fb2da0ee5e0eaaf74634860caf247d
Art reyrey <cuizonlance49@gmail.com>
2025-02-08 17:09:02 +0800

    Add files via upload

---
+++ Lesson1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    double num1, num2;
+    char operation;
+    string choice;
+
+    do {
+        cout << "Start or End? (start/end): ";
+        cin >> choice;
+
+        if (choice == "end") {
+            cout << "Niga Rick";
+            break;
+        }
+
+        cout << "Input number 1: ";
+        cin >> num1;
+
+        cout << "Input number 2: ";
+        cin >> num2;
+
+        cout << "Input operation (*, +, -, /): ";
+        cin >> operation;
+
+        if (operation == '+') {
+            cout << "Result: " << num1 + num2 << endl;
+        }
+        else if (operation == '-') {
+            cout << "Result: " << num1 - num2 << endl;
+        }
+        else if (operation == '*') {
+            cout << "Result: " << num1 * num2 << endl;
+        }
+
+        else {
+            cout << "Invalid input. Please try again." << endl;
+        }
+    } while (choice != "end");
+
+    return 0;
+    //deo ang pogi 246810
+}
